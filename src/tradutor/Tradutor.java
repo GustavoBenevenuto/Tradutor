d6e6ba215cd2147dde9c5048933c757617635861
Gustavo Benevenuto <gustavo.benesilva@gmail.com>
2019-05-24 10:23:10 -0300

    Primeira Versão

---
+++ src/tradutor/Tradutor.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tradutor;
+
+import javax.swing.JOptionPane;
+import view.FrmTelaPrincipal;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Tradutor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmTelaPrincipal frmTela = new FrmTelaPrincipal();
+        frmTela.show();
+    }
+    
+}
